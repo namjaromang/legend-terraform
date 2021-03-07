da095851a0385c254ec9a66fb1f3a20df10951d8
` <yjpark@onemedicsindustry.com>
2021-03-07 22:32:45 +0900

    1주차

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # legend-terraform
 The Legend team does terraform research
+
+### - [1주차 왜 테라폼인가?](docs/1주차.md)
+
+### - [2주차 테라폼 시작하기](docs/2주차.md)
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws-seoul" {
+  access_key = "AKIAQLQYUTXEGOOC6EWW"
+  secret_key = "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"
+  region = "ap-northeast-2"
+}
